b86343f57ee85d9d58619fc8eb1fb2f012210346
catharsis96 <catharsis96@yandex.ru>
2017-01-03 15:03:54 +0300

    passed

--- lesson6/task3/while_loop.py
+++ lesson6/task3/while_loop.py
@@ -1,7 +1,7 @@
 square = 1
 
 while square <= 10:
-    print(square)    # This code is executed 10 times
+#    print(square)    # This code is executed 10 times
     square += 1      # This code is executed 10 times
 
 print("Finished")  # This code is executed once
@@ -9,7 +9,8 @@ print("Finished")  # This code is executed once
 square = 0
 number = 1
 
-print all squares from 0 to 99
+while square < 79:
     square = number ** 2
     print(square)
     number += 1
+
--- lesson7/task2/param_args.py
+++ lesson7/task2/param_args.py
@@ -3,11 +3,13 @@ def foo(x):                 # x is a function parameter
 
 foo(5)   # pass 5 to foo(). Here 5 is an argument passed to function foo.
 
-define a function named 'square' that prints square of passed parameter
+def square(x):
     print(x ** 2)
 
-square(4)
+square(9)
 square(8)
 square(15)
 square(23)
 square(42)
+
+
